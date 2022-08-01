ffce3b288d84ef138b0415dbebc2a5aa5d5259de
stevencorrea-chicago <40609698+stevencorrea-chicago@users.noreply.github.com>
2022-08-01 08:27:41 -0500

    Update amazing_grace_lyrics.py

--- amazing_grace_lyrics.py
+++ amazing_grace_lyrics.py
@@ -1,19 +1,19 @@
-print ("Amazing grace! how sweet the sound,\
-  That saved a wretch; like me!\
-I once was lost, but now am found,\
-  Was blind, but now I see.\
-2\
-’Twas grace that taught my heart to fear,\
-  And grace my fears relieved;\
-How precious did that grace appear\
-  The hour I first believed!\
-3\
-The Lord hath promised good to me,\
-  His word my hope secures;\
-He will my shield and portion be\
-  As long as life endures.\
-4\
-When we’ve been there ten thousand years,\
-  Bright shining as the sun,\
-We’ve no less days to sing God’s praise\
+print ("Amazing grace! how sweet the sound,\n\
+  That saved a wretch; like me!\n\
+I once was lost, but now am found,\n\
+  Was blind, but now I see.\n\
+2\n\
+’Twas grace that taught my heart to fear,\n\
+  And grace my fears relieved;\n\
+How precious did that grace appear\n\
+  The hour I first believed!\n\
+3\n\
+The Lord hath promised good to me,\n\
+  His word my hope secures;\n\
+He will my shield and portion be\n\
+  As long as life endures.\n\
+4\n\
+When we’ve been there ten thousand years,\n\
+  Bright shining as the sun,\n\
+We’ve no less days to sing God’s praise\n\
   Than when we first begun.")
