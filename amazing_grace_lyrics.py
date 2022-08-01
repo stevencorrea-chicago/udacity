d7748c428bb7ac7ad2f05c1a6ddf3d55ab4dab47
stevencorrea-chicago <40609698+stevencorrea-chicago@users.noreply.github.com>
2022-08-01 08:06:39 -0500

    print_amazing_grace
    Amazing Grace lyrics

---
+++ amazing_grace_lyrics.py
@@ -0,0 +1,19 @@
+print ("Amazing grace! how sweet the sound,\
+  That saved a wretch; like me!\
+I once was lost, but now am found,\
+  Was blind, but now I see.\
+2\
+’Twas grace that taught my heart to fear,\
+  And grace my fears relieved;\
+How precious did that grace appear\
+  The hour I first believed!\
+3\
+The Lord hath promised good to me,\
+  His word my hope secures;\
+He will my shield and portion be\
+  As long as life endures.\
+4\
+When we’ve been there ten thousand years,\
+  Bright shining as the sun,\
+We’ve no less days to sing God’s praise\
+  Than when we first begun.")
