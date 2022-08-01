a21790e5f98852b6d3b73ffe80f94a54cc9a9d4f
stevencorrea-chicago <40609698+stevencorrea-chicago@users.noreply.github.com>
2022-08-01 08:09:27 -0500

    Create sabbath_bloody_sabbath.py
    sabbath_bloody_sabbath lyrics

---
+++ sabbath_bloody_sabbath.py
@@ -0,0 +1,41 @@
+print("You've seen life through distorted eyes\n\
+You know you had to learn\n\
+The execution of your mind\n\
+You really had to turn\n\
+The race is run the book is read\n\
+The end begins to show\n\
+The truth is out, the lies are old\n\
+But you don't want to know\n\
+Nobody will ever let you know\n\
+When you ask the reasons why\n\
+They just tell you that you're on your own\n\
+Fill your head all full of lies\n\
+The people who have crippled you\n\
+You want to see them burn\n\
+The gates of life have closed on you\n\
+And now there's just no return\n\
+You're wishing that the hands of doom\n\
+Could take your mind away\n\
+And you don't care if you don't see again\n\
+The light of day\n\
+Nobody will ever let you know\n\
+When you ask the reasons why\n\
+They just tell you that you're on your own\n\
+Fill your head all full of lies\n\
+You bastards\n\
+Where can you run to\n\
+What more can you do\n\
+No more tomorrow\n\
+Life is killing you\n\
+Dreams turn to nightmares\n\
+Heaven turns to hell\n\
+Burned out confusion\n\
+Nothing more to tell, yeah\n\
+Everything around you\n\
+What's it coming to\n\
+God knows as your dog knows\n\
+Bog blast all of you\n\
+Sabbath bloody Sabbath\n\
+Nothing more to do\n\
+Living just for dying\n\
+Dying just for you, yeah")
