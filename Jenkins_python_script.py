c94ba17306651c96a4522b66be40c1dc5b70f7b1
stevencorrea-chicago <40609698+stevencorrea-chicago@users.noreply.github.com>
2022-08-31 08:07:24 -0500

    Update Jenkins_python_script.py
    Added step to make temp directory

--- Jenkins_python_script.py
+++ Jenkins_python_script.py
@@ -1,4 +1,10 @@
 import sys
+import os
 
 for i in range(1, len(sys.argv)):
     print('argument:', i, 'value:', sys.argv[i])
+    
+try: 
+    os.mkdir(/home/steve/Python/DeleteMe) 
+except OSError as error: 
+    print(error)  
