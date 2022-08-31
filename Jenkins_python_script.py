6bc1c557ab01a35d83e960443e885999bc245466
stevencorrea-chicago <40609698+stevencorrea-chicago@users.noreply.github.com>
2022-08-31 08:33:43 -0500

    Update Jenkins_python_script.py

--- Jenkins_python_script.py
+++ Jenkins_python_script.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import shutil
 
 for i in range(1, len(sys.argv)):
     print('argument:', i, 'value:', sys.argv[i])
@@ -8,3 +9,5 @@ try:
     os.mkdir("/home/steve/Python/DeleteMe") 
 except OSError as error: 
     print(error)  
+
+shutil.chown("/home/steve/Python/DeleteMe", 1000, 1000)
